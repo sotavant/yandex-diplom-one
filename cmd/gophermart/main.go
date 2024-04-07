86581aed5906636b2666881476e914671dc592b8
sotavant <fans7288@gmail.com>
2024-04-07 23:21:03 +0500

    beginning

--- cmd/gophermart/main.go
+++ cmd/gophermart/main.go
@@ -1,3 +1,14 @@
 package main
 
-func main() {}
+import (
+	"context"
+	"github.com/sotavant/yandex-diplom-one/internal"
+)
+
+func main() {
+	ctx := context.Background()
+	app, err := internal.InitApp(ctx)
+	if err != nil {
+		panic(err)
+	}
+}
---
+++ internal/app.go
@@ -0,0 +1,94 @@
+package internal
+
+import (
+	"context"
+	"errors"
+	"flag"
+	"github.com/jackc/pgx/v5/pgxpool"
+	"os"
+)
+
+const (
+	runAddressVar        = "RUN_ADDRESS"
+	databaseURIVar       = "DATABASE_URI"
+	accrualSystemAddrVar = "ACCRUAL_SYSTEM_ADDRESS"
+)
+
+type App struct {
+	Address           string
+	AccrualSysAddress string
+	DBPool            *pgxpool.Pool
+}
+
+type config struct {
+	runAddress        string
+	databaseURI       string
+	accrualSystemAddr string
+}
+
+func InitApp(ctx context.Context) (*App, error) {
+	c := initConfig()
+	if err := checkConfig(c); err != nil {
+		return nil, err
+	}
+
+	dbPool, err := initDB(ctx, c.databaseURI)
+	if err != nil {
+		return nil, err
+	}
+
+	return &App{
+		Address:           c.runAddress,
+		AccrualSysAddress: c.accrualSystemAddr,
+		DBPool:            dbPool,
+	}, nil
+}
+
+func initConfig() *config {
+	c := new(config)
+
+	flag.StringVar(&c.runAddress, "a", "", "server address")
+	flag.StringVar(&c.databaseURI, "d", "", "database uri")
+	flag.StringVar(&c.accrualSystemAddr, "r", "", "accrual system address")
+
+	if envVar := os.Getenv(runAddressVar); envVar != "" {
+		c.runAddress = envVar
+	}
+
+	if envVar := os.Getenv(databaseURIVar); envVar != "" {
+		c.databaseURI = envVar
+	}
+
+	if envVar := os.Getenv(accrualSystemAddrVar); envVar != "" {
+		c.accrualSystemAddr = envVar
+	}
+
+	return c
+}
+
+func initDB(ctx context.Context, DSN string) (*pgxpool.Pool, error) {
+	dbConf, err := pgxpool.ParseConfig(DSN)
+	if err != nil {
+		return nil, err
+	}
+
+	dbPool, err := pgxpool.NewWithConfig(ctx, dbConf)
+	if err != nil {
+		return nil, err
+	}
+
+	err = dbPool.Ping(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	return dbPool, nil
+}
+
+func checkConfig(c *config) error {
+	if c.runAddress == "" || c.databaseURI == "" || c.accrualSystemAddr == "" {
+		return errors.New("please, check configs")
+	}
+
+	return nil
+}
